adc4e6da42bf01e48ba77a24a384458750d65bc7
Alexis Tual <atual@gradle.com>
2024-04-26 14:46:38 +0200

    Add support for short lived tokens

--- sources/src/build-scan.ts
+++ sources/src/build-scan.ts
@@ -1,7 +1,8 @@
 import * as core from '@actions/core'
 import {BuildScanConfig} from './configuration'
+import {getToken} from './shortlived-token/shortlived-token'
 
-export function setup(config: BuildScanConfig): void {
+export async function setup(config: BuildScanConfig): Promise<void> {
     maybeExportVariable('DEVELOCITY_INJECTION_INIT_SCRIPT_NAME', 'gradle-actions.inject-develocity.init.gradle')
     maybeExportVariable('DEVELOCITY_AUTO_INJECTION_CUSTOM_VALUE', 'gradle-actions')
     if (config.getBuildScanPublishEnabled()) {
@@ -11,6 +12,29 @@ export function setup(config: BuildScanConfig): void {
         maybeExportVariable('DEVELOCITY_TERMS_OF_USE_URL', config.getBuildScanTermsOfUseUrl())
         maybeExportVariable('DEVELOCITY_TERMS_OF_USE_AGREE', config.getBuildScanTermsOfUseAgree())
     }
+    if (config.getDevelocityAccessKey()) {
+        try {
+            core.debug('Fetching short-lived token...')
+            const tokens = await getToken(
+                getEnv('DEVELOCITY_ENFORCE_URL'),
+                getEnv('DEVELOCITY_URL'),
+                config.getDevelocityAccessKey(),
+                config.getDevelocityTokenExpiry()
+            )
+            if (tokens != null && !tokens.isEmpty()) {
+                core.debug(`Got token(s), setting the DEVELOCITY_ACCESS_KEY env var`)
+                const token = tokens.raw()
+                core.setSecret(token)
+                core.exportVariable('DEVELOCITY_ACCESS_KEY', token)
+            }
+        } catch (e) {
+            core.warning(`Failed to fetch short-lived token, reason: ${e}`)
+        }
+    }
+}
+
+function getEnv(variableName: string): string | undefined {
+    return process.env[variableName]
 }
 
 function maybeExportVariable(variableName: string, value: unknown): void {
